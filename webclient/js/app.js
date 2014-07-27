270968a36d0b8e2ef36e11d579762dc56f632d93
Chris Proctor <chris.proctor@gmail.com>
2014-07-27 11:27:15 -0400

    Adding webclient project

---
+++ webclient/js/app.js
@@ -0,0 +1,11 @@
+app = angular.module('nuzzleClient')
+
+app.controller('nuzzleController', function($scope) {
+    
+})
+
+app.service('nuzzleApi', ['$http', function($http) {
+    this.getMessages = 
+    this.getMessage = 
+
+})
